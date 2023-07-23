6885c25dfbf59f6c1df600066bd30f33fb2f2e0f
Gegerout <selimchik06@gmail.com>
2023-07-23 20:05:58 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/data/data_sources/local_data.dart
+++ lib/home/data/data_sources/local_data.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:location_app/home/data/models/image_model.dart';
@@ -35,19 +36,76 @@ class LocalData {
   }
 
   Future<ImageModel> getCityDataFromImage(String id) async {
+    // var dir = await getTemporaryDirectory();
+    // final File imageLocationData = File("${dir.path}/imagesLocationData.json");
+    // const apiUrl = "http://evgeniymuravyov.pythonanywhere.com/getLocation";
+    // final Dio dio = Dio();
+    //
+    // final latLongData = await getLocationDataFromImage(id);
+    // final res = await dio.get(apiUrl, queryParameters: {
+    //   "longitude": latLongData.longitude,
+    //   "latitude": latLongData.latitude,
+    // }, options: Options(receiveTimeout: const Duration(seconds: 60)));
+    //
+    // final model =
+    //     ImageModel(id, latLongData.longitude!, latLongData.latitude!, res.data);
+    // return model;
+    const apiUrl = "https://geocode-maps.yandex.ru/1.x/";
+    final Dio dio = Dio();
     var dir = await getTemporaryDirectory();
     final File imageLocationData = File("${dir.path}/imagesLocationData.json");
-    const apiUrl = "http://evgeniymuravyov.pythonanywhere.com/getLocation";
-    final Dio dio = Dio();
+    List data = [];
+    double lastLon = 0.0;
+    double lastLat = 0.0;
+    Map<String, dynamic> lastLocation = {
+      "country": "",
+      "city": ""
+    };
+
+    if(imageLocationData.existsSync()) {
+      data = json.decode(imageLocationData.readAsStringSync());
+      lastLon = data.last["longitude"];
+      lastLat = data.last["latitude"];
+      lastLocation = data.last["locationData"];
+    }
 
     final latLongData = await getLocationDataFromImage(id);
-    final res = await dio.get(apiUrl, queryParameters: {
-      "longitude": latLongData.longitude,
-      "latitude": latLongData.latitude,
-    }, options: Options(receiveTimeout: const Duration(seconds: 60)));
-
-    final model =
-        ImageModel(id, latLongData.longitude!, latLongData.latitude!, res.data);
-    return model;
+    if((latLongData.longitude! - lastLon).abs() < 0.1 && (latLongData.latitude! - lastLat).abs() < 0.1) {
+      lastLon = latLongData.longitude!;
+      lastLat = latLongData.latitude!;
+      final model = ImageModel(id, lastLon, lastLat, lastLocation);
+      data.add(model);
+      imageLocationData.writeAsStringSync(json.encode(data));
+      return model;
+    } else {
+      final res = await dio.get(apiUrl, queryParameters: {
+        "apikey": "a4048f27-af7e-474c-af42-92cd0a03eccb",
+        "geocode": "${latLongData.longitude},${latLongData.latitude}",
+        "format": "json"
+      });
+      final List answer = res.data["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["metaDataProperty"][
+      "GeocoderMetaData"]["text"].split(",");
+      lastLon = latLongData.longitude!;
+      lastLat = latLongData.latitude!;
+      if(answer.length >= 2) {
+        lastLocation = {
+          "country": answer[0],
+          "city": answer[1]
+        };
+        final model = ImageModel(id, lastLon, lastLat, lastLocation);
+        data.add(model);
+        imageLocationData.writeAsStringSync(json.encode(data));
+        return model;
+      } else {
+        lastLocation = {
+          "country": answer[0],
+          "city": ""
+        };
+        final model = ImageModel(id, lastLon, lastLat, lastLocation);
+        data.add(model);
+        imageLocationData.writeAsStringSync(json.encode(data));
+        return model;
+      }
+    }
   }
 }
