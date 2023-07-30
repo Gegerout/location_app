1e5eef49fed2361daad4c53b0f6a473dcba54f11
Gegerout <selimchik06@gmail.com>
2023-07-30 22:48:53 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/presentation/widgets/image_thumbnail_widget.dart
+++ lib/home/presentation/widgets/image_thumbnail_widget.dart
@@ -7,6 +7,7 @@ import 'package:photo_manager/photo_manager.dart';
 Widget imageThumbnailWidget(AssetEntity asset, Uint8List? thumbData,
     Map<String, dynamic> locationData, WidgetRef ref) {
   if (thumbData != null) {
+    print(thumbData);
     return SingleChildScrollView(
       child: Column(
         children: [
