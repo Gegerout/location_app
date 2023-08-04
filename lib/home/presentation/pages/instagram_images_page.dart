674654fd8e739ba92980fefb9db5be0c94352d08
Gegerout <selimchik06@gmail.com>
2023-08-04 17:32:59 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/presentation/pages/instagram_images_page.dart
+++ lib/home/presentation/pages/instagram_images_page.dart
@@ -12,28 +12,36 @@ class InstagramImagesPage extends ConsumerWidget {
         title: const Text("Instagram Images Page"),
       ),
       body: ref.watch(getLocationsProvider).when(
-              data: (value) {
-                return ListView.builder(
-                  itemCount: value.length,
-                  itemBuilder: (context, index) {
-                    return Text(value[index]);
-                  },
-                );
-              },
-              error: (error, stacktrace) {
-                return AlertDialog(
-                          title: Text(error.toString()),
-                          actions: [
-                            ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text("Ok"))
-                          ],
-                        );
-              },
-              loading: () => const Center(
-                      child: CircularProgressIndicator(),
+          data: (value) {
+            return Center(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: value.length,
+                itemBuilder: (context, index) {
+                  return Center(
+                      child: Text(
+                    value[index],
+                    style: const TextStyle(
+                        fontSize: 24, fontWeight: FontWeight.w500),
+                  ));
+                },
+              ),
+            );
+          },
+          error: (error, stacktrace) {
+            return AlertDialog(
+              title: Text(error.toString()),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Ok"))
+              ],
+            );
+          },
+          loading: () => const Center(
+                child: CircularProgressIndicator(),
               )),
     );
   }
