3c569def9898c37aac6a3863345d700c37cd2a90
Gegerout <selimchik06@gmail.com>
2023-08-09 17:22:12 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/presentation/pages/home_page.dart
+++ lib/home/presentation/pages/home_page.dart
@@ -1,16 +1,65 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:location_app/home/presentation/providers/get_images_provider.dart';
 
 import '../../../auth/data/models/user_model.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends ConsumerWidget {
   const HomePage({Key? key, required this.userModel}) : super(key: key);
 
   final UserModel userModel;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: SafeArea(child: Text(userModel.username)),
+      appBar: AppBar(
+        title: const Text("Home page"),
+      ),
+      body: Column(
+        children: [
+          Text(
+            userModel.username,
+            style: const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+          ),
+          ref.watch(getImagesFromProfileProvider(userModel.accessToken)).when(
+              data: (value) {
+                if (value != null) {
+                  return GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 4),
+                      itemCount: value.length,
+                      itemBuilder: (context, index) {
+                        return SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: CachedNetworkImage(
+                              imageUrl: value[index].mediaUrl,
+                              fit: BoxFit.fill,
+                            ));
+                      });
+                }
+                return const Text("Something went wrong");
+              },
+              error: (error, stacktrace) {
+                return AlertDialog(
+                  title: Text(error.toString()),
+                  actions: [
+                    ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Ok"))
+                  ],
+                );
+              },
+              loading: () => const Center(
+                    child: CircularProgressIndicator(),
+                  ))
+        ],
+      ),
     );
   }
 }
