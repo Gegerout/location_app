06374525d010e4e000f90b1b2efff2b447deb50c
Gegerout <selimchik06@gmail.com>
2023-08-11 16:56:33 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/presentation/pages/locations_page.dart
+++ lib/home/presentation/pages/locations_page.dart
@@ -16,49 +16,51 @@ class LocationsPage extends ConsumerWidget {
         title: const Text("Locations page"),
       ),
       body: ref.watch(getPostsDataProvider(userModel.accessToken)).when(
-              data: (value) {
-                if(value != null) {
-                  if(value.locationData != null) {
-                    List cities = [];
-                    List countries = [];
+          data: (value) {
+            if (value != null) {
+              if (value.locationData != null) {
+                List cities = [];
+                List countries = [];
 
-                    for (var element in value.locationData!.data) {
-                      final location = element.loadedLocation.split(",");
-                      if(location.length >= 2) {
-                        if(!cities.contains(location[1])) {
-                          cities.add(location[1]);
-                          countries.add(location[0]);
-                        }
-                      } else {
-                        cities.add(location[0]);
-                        countries.add("");
-                      }
+                for (var element in value.locationData!.data) {
+                  final location = element.loadedLocation.split(",");
+                  if (location.length >= 2) {
+                    if (!cities.contains(location[1])) {
+                      cities.add(location[1]);
+                      countries.add(location[0]);
                     }
-
-                    return ListView.builder(
-                      itemCount: cities.length,
-                      itemBuilder: (context, index) {
-                        return Text("${cities[index]}, ${countries[index]}");
-                      },
-                    );
+                  } else {
+                    cities.add(location[0]);
+                    countries.add("");
                   }
                 }
-                return const Text("Something went wrong");
-              },
-              error: (error, stacktrace) {
-                return AlertDialog(
-                          title: Text(error.toString()),
-                          actions: [
-                            ElevatedButton(
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
-                                child: const Text("Ok"))
-                          ],
-                        );
-              },
-              loading: () => const Center(
-                      child: CircularProgressIndicator(),
+
+                return ListView.builder(
+                  itemCount: cities.length,
+                  itemBuilder: (context, index) {
+                    return Text("${cities[index]}, ${countries[index]}",
+                        style: const TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600));
+                  },
+                );
+              }
+            }
+            return const Text("Something went wrong");
+          },
+          error: (error, stacktrace) {
+            return AlertDialog(
+              title: Text(error.toString()),
+              actions: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text("Ok"))
+              ],
+            );
+          },
+          loading: () => const Center(
+                child: CircularProgressIndicator(),
               )),
     );
   }
