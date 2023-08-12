7b9277eb75eecd7645982a531e16f2dff3eb579b
Gegerout <selimchik06@gmail.com>
2023-08-12 20:38:39 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/presentation/pages/locations_page.dart
+++ lib/home/presentation/pages/locations_page.dart
@@ -27,7 +27,9 @@ class LocationsPage extends ConsumerWidget {
                   if (location.length >= 2) {
                     if (!cities.contains(location[1])) {
                       cities.add(location[1]);
-                      countries.add(location[0]);
+                      if (!countries.contains(location[0])) {
+                        countries.add(location[0]);
+                      }
                     }
                   } else {
                     cities.add(location[0]);
@@ -35,14 +37,65 @@ class LocationsPage extends ConsumerWidget {
                   }
                 }
 
-                return ListView.builder(
-                  itemCount: cities.length,
-                  itemBuilder: (context, index) {
-                    return Text("${cities[index]}, ${countries[index]}",
-                        style: const TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.w600));
-                  },
-                );
+                String cityForm = "городов";
+                String countryForm = "странах";
+
+                if (cities.length.toString().endsWith("1")) {
+                  cityForm = "город";
+                  countryForm = "стране";
+                } else if (cities.length.toString().endsWith("2") ||
+                    cities.length.toString().endsWith("3") ||
+                    cities.length.toString().endsWith("4")) {
+                  cityForm = "городa";
+                }
+
+                  return SingleChildScrollView(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Вы посетили ${cities.length} $cityForm:",
+                              style: const TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w600)),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: cities.length,
+                              itemBuilder: (context, index) {
+                                return Text(
+                                    cities[index]
+                                        .toString()
+                                        .replaceFirst(" ", ""),
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500));
+                              },
+                            ),
+                          ),
+                          Text("в ${countries.length} $countryForm:",
+                              style: const TextStyle(
+                                  fontSize: 24, fontWeight: FontWeight.w600)),
+                          Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: countries.length,
+                              itemBuilder: (context, index) {
+                                return Text(countries[index],
+                                    style: const TextStyle(
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.w500));
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
               }
             }
             return const Text("Something went wrong");
