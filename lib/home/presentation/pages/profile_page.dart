301dd2bf2caf1fc3b8ebb4a3becc847647c99c88
Gegerout <selimchik06@gmail.com>
2023-08-14 16:09:50 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/home/presentation/pages/profile_page.dart
+++ lib/home/presentation/pages/profile_page.dart
@@ -16,82 +16,85 @@ class ProfilePage extends ConsumerWidget {
       appBar: AppBar(
         title: const Text("Profile page"),
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              children: [
-                ClipRRect(
-                    borderRadius: BorderRadius.circular(50),
-                    child: SizedBox(
-                        width: 100,
-                        height: 100,
-                        child: CachedNetworkImage(
-                            imageUrl: userModel.profilePicture))),
-                const SizedBox(width: 20,),
-                Text(
-                  userModel.username,
-                  style:
-                      const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: SizedBox(
+                          width: 100,
+                          height: 100,
+                          child: CachedNetworkImage(
+                              imageUrl: userModel.profilePicture))),
+                  const SizedBox(width: 20,),
+                  Text(
+                    userModel.username,
+                    style:
+                        const TextStyle(fontWeight: FontWeight.w500, fontSize: 20),
+                  ),
+                ],
+              ),
             ),
-          ),
-          ref.watch(getPostsDataProvider(userModel.accessToken)).when(
-              data: (value) {
-                if (value != null) {
-                  if (value.$1?.imagesData != null) {
-                    return GridView.builder(
-                        shrinkWrap: true,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 4),
-                        itemCount: value.$1!.imagesData!.data.length,
-                        itemBuilder: (context, index) {
-                          return SizedBox(
-                              height: 100,
-                              width: 100,
-                              child: CachedNetworkImage(
-                                imageUrl:
-                                    value.$1!.imagesData!.data[index].mediaUrl,
-                                fit: BoxFit.cover,
-                                progressIndicatorBuilder:
-                                    (context, url, downloadProgress) => Center(
-                                  child: SizedBox(
-                                    height: 40,
-                                    width: 40,
-                                    child: CircularProgressIndicator(
-                                        value: downloadProgress.progress),
+            ref.watch(getPostsDataProvider(userModel.accessToken)).when(
+                data: (value) {
+                  if (value != null) {
+                    if (value.$1?.imagesData != null) {
+                      return GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 4),
+                          itemCount: value.$1!.imagesData!.data.length,
+                          itemBuilder: (context, index) {
+                            return SizedBox(
+                                height: 100,
+                                width: 100,
+                                child: CachedNetworkImage(
+                                  imageUrl:
+                                      value.$1!.imagesData!.data[index].mediaUrl,
+                                  fit: BoxFit.cover,
+                                  progressIndicatorBuilder:
+                                      (context, url, downloadProgress) => Center(
+                                    child: SizedBox(
+                                      height: 40,
+                                      width: 40,
+                                      child: CircularProgressIndicator(
+                                          value: downloadProgress.progress),
+                                    ),
                                   ),
-                                ),
-                                errorWidget: (context, url, error) =>
-                                    const Icon(
-                                  Icons.error,
-                                  color: Colors.redAccent,
-                                ),
-                              ));
-                        });
+                                  errorWidget: (context, url, error) =>
+                                      const Icon(
+                                    Icons.error,
+                                    color: Colors.redAccent,
+                                  ),
+                                ));
+                          });
+                    }
                   }
-                }
-                return const Text("Something went wrong");
-              },
-              error: (error, stacktrace) {
-                return AlertDialog(
-                  title: Text(error.toString()),
-                  actions: [
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text("Ok"))
-                  ],
-                );
-              },
-              loading: () => const Center(
-                    child: CircularProgressIndicator(),
-                  ))
-        ],
+                  return const Text("Something went wrong");
+                },
+                error: (error, stacktrace) {
+                  return AlertDialog(
+                    title: Text(error.toString()),
+                    actions: [
+                      ElevatedButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          child: const Text("Ok"))
+                    ],
+                  );
+                },
+                loading: () => const Center(
+                      child: CircularProgressIndicator(),
+                    ))
+          ],
+        ),
       ),
     );
   }
