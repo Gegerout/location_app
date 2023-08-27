1bf4cef0dfef6030168d369c882dac6df12ccc51
Gegerout <selimchik06@gmail.com>
2023-08-27 13:28:03 +0300

    Loading images and getting location from them feature with all images with locations (city and country) displayed

--- lib/auth/presentation/pages/add_instagram_page.dart
+++ lib/auth/presentation/pages/add_instagram_page.dart
@@ -12,52 +12,56 @@ class AddInstagramPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
         body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-                width: double.infinity,
-                child: Image.asset(
-                  "assets/images/add_inst_top.png",
-                  scale: 4,
-                  fit: BoxFit.cover,
-                )),
-            Padding(
-              padding: const EdgeInsets.only(left: 36, right: 36, top: 80),
-              child: Column(
-                children: [
-                  const Text("Add Instagram\nAccount", style: TextStyle(fontSize: 64,
-                      fontFamily: "Futura BT",
-                      height: 1,
-                      fontWeight: FontWeight.w900,
-                      color: Colors.black),),
-                  const SizedBox(height: 80),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 60,
-                    child: ElevatedButton(
-                        onPressed: () {
-                          ref.read(signupProvider.notifier).addInstagramAccount();
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoadingPage(email)));
-                        },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFFF80261),
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20))),
-                        child: const Text("Add account",
-                            style: TextStyle(
-                                fontWeight: FontWeight.w900,
-                                fontFamily: "Futura BT",
-                                fontSize: 22,
-                                color: Colors.white))),
-                  ),
-                ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(
+            width: double.infinity,
+            child: Image.asset(
+              "assets/images/add_inst_top.png",
+              scale: 4,
+              fit: BoxFit.cover,
+            )),
+        Padding(
+          padding: const EdgeInsets.only(left: 36, right: 36, top: 80),
+          child: Column(
+            children: [
+              const Text(
+                "Add Instagram\nAccount",
+                style: TextStyle(
+                    fontSize: 64,
+                    fontFamily: "Futura BT",
+                    height: 1,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black),
               ),
-            ),
-          ],
-        ));
+              const SizedBox(height: 80),
+              SizedBox(
+                width: double.infinity,
+                height: 60,
+                child: ElevatedButton(
+                    onPressed: () {
+                      ref.read(signupProvider.notifier).addInstagramAccount();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => LoadingPage(email)));
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFFF80261),
+                        elevation: 0,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    child: const Text("Add account",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w900,
+                            fontFamily: "Futura BT",
+                            fontSize: 22,
+                            color: Colors.white))),
+              ),
+            ],
+          ),
+        ),
+      ],
+    ));
   }
 }
